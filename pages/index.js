a1f69d1973f488d190dda6fb24ec84ab719de60f
Wallace Herlon <wherlon@hotmail.com>
2022-06-26 12:57:06 -0300

    Filtra dados

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ const Index = (props) => {
             <div>{props.currentDate}</div>
             {props.repos.map(repo => {
                 return (
-                    <div>
+                    <div key={repo.id}>
                         <h3>{repo.full_name}</h3>
                         <p>Language: {repo.language} / Starts: {repo.stargazers_count}</p>
                     </div>
@@ -18,7 +18,25 @@ const Index = (props) => {
 }
 export async function getServerSideProps(context) {
     const resRepos = await fetch('https://api.github.com/users/wallaceherlon/repos?sort=updated')
-    const repos = await resRepos.json()
+    const originalRepos = await resRepos.json()
+
+    //repositório que não deseja mostrar 
+    const dontShowRepos = ['WallaceHerlon/Banco-de-dados', 'WallaceHerlon/WallaceHerlon']
+    const dontShowFilter = repo => dontShowRepos.indexOf(repo.full_name) === -1
+    //repositório fork para não mostrar
+    const isNotFork = repo => !repo.fork
+    //Não mandar os dados no final da página, limpando os dados
+    const extractData = repo => ({
+        id: repo.id,
+        full_name: repo.full_name,
+        language: repo.language,
+        stargazers_count: repo.stargazers_count
+    })
+
+    const repos = originalRepos
+        .filter(isNotFork)
+        .filter(dontShowFilter)
+        .map(extractData)
     return {
         props: {
             currentDate: new Date().toString(),
