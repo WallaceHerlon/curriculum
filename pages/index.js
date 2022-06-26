9ceddbc562630bb8be9a4b9cfac209ea22d0107c
Wallace Herlon <wherlon@hotmail.com>
2022-06-26 12:26:07 -0300

    Listando repositórios

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,29 @@
 import React from 'react'
 
-const Index = () => {
+const Index = (props) => {
     return (
         <div>
             <h1>Bem-vindo!</h1>
+            <div>{props.currentDate}</div>
+            {props.repos.map(repo => {
+                return (
+                    <div>
+                        <h3>{repo.full_name}</h3>
+                        <p>Language: {repo.language} / Starts: {repo.stargazers_count}</p>
+                    </div>
+                )
+            })}
         </div>
     )
 }
-
+export async function getServerSideProps(context) {
+    const resRepos = await fetch('https://api.github.com/users/wallaceherlon/repos?sort=updated')
+    const repos = await resRepos.json()
+    return {
+        props: {
+            currentDate: new Date().toString(),
+            repos
+        }
+    }
+}
 export default Index
