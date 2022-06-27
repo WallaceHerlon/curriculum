bd8e69dc0cd5dd6c0d5735e6df671eb72828b71e
Wallace Herlon <wherlon@hotmail.com>
2022-06-26 21:18:18 -0300

    Tailwindcss correção
    Para funcionamento do tailwind necessário criar o arquivo dentro da pasta pages o _app.js, arquivo de propagação.

---
+++ pages/_app.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import '../css/style.css'
+
+const MyApp = ({ Component, pageProps }) => {
+    return (
+        <Component {...pageProps} />
+
+    )
+}
+
+export default MyApp
