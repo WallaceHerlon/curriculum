8cfdcb2f8aec48eab8ee7562febd883fd467e0ef
Wallace Herlon <wherlon@hotmail.com>
2022-06-28 22:06:56 -0300

    PurgeCSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,19 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+const purgecss = [
+  '@fullhuman/postcss-purgecss',
+  {
+    content: ['./components/**/*.js', './pages/**/*.js'],
+    whitelist: ['body', 'html'],
+    whitelistPatterns: [/psv-/],
+    whitelistPatternsChildren: [/psv-/],
+    defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
   },
+]
+
+module.exports = {
+  plugins: [
+    'postcss-import',
+    'tailwindcss',
+    'autoprefixer',
+    ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
+  ],
 }
