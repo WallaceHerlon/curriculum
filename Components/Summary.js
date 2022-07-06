a87a9cafe3ff44d59235c29ad66fedb5abf9e34f
Wallace Herlon <wherlon@hotmail.com>
2022-07-06 20:19:28 -0300

    Alterando configuração dos pacote tailwind

--- Components/Summary.js
+++ Components/Summary.js
@@ -4,8 +4,8 @@ const Summary = () => {
     return (
         <div className='bg-white rounded-lg shadow-lg py-12 px-8 mx-8 md:mx-0 md:px-16 '>
         <p className='text-2xl'>Técnico em eletrônica atuando na área de manutenção de ATM. 
-            Formado em analise e desenvolvimento de sistema, estudando Javascript, focado em Node | React | NextJS.
-            Objetivo realizar transição de carreia, devido afinidade com sistema como técnico hoje quero cria soluções uma dev!  
+            Formado em análise e desenvolvimento de sistema, estudando Javascript, focado em Node | React | NextJS.
+            Objetivo realizar transição de carreia, tendo afinidade com sistema como técnico hoje quero cria soluções uma desenvolvedor!  
         </p>
     </div>
     )
